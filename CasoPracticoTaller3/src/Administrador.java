e3f03be67d023c5d8ca21bbd00f5d17424ba8921
Antonio Cuñat Alario <Antonio.Cunat-Alario@uv.es>
2019-06-08 18:30:28 +0200

    Afegida classe Administrador

---
+++ CasoPracticoTaller3/src/Administrador.java
@@ -0,0 +1,15 @@
+
+public class Administrador extends UsuarioBase {
+
+	public Administrador(String nombre, String apellidos) {
+		super(nombre, apellidos);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean isAdministrador() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
